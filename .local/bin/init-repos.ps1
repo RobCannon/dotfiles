963f51fec11bddeee5f78f0f71617c85ba478e79
Rob Cannon <rob.cannon@equifax.com>
2021-01-05 14:40:11 +0000

    More files

---
+++ .local/bin/init-repos.ps1
@@ -0,0 +1,6 @@
+#!/bin/pwsh
+clone-repos.ps1 cloud-services-equifax-shell src/equifax-shell
+clone-repos.ps1 cloud-services-container-imaging src/container-imaging
+clone-repos.ps1 cloud-services-kubernetes src/kubernetes
+clone-repos.ps1 cloud-services-kubernetes src/cloud-services
+clone-repos.ps1 iaas-platform-services-fr-smtp-relay src/smtp-relay
