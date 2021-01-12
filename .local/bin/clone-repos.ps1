04a00874e7442996d171104a608f2a520b0b9883
Rob Cannon <rob.cannon@equifax.com>
2021-01-12 20:43:38 +0000

    Made clone repos use better folder structure

--- .local/bin/clone-repos.ps1
+++ .local/bin/clone-repos.ps1
@@ -3,22 +3,30 @@ Param (
   [Parameter(Mandatory=$true)]
   [string]$TeamName,
 
-  [Parameter(Mandatory=$true)]
   [string]$Path
 )
 
-$token=(Get-Content ~/.config/gh/hosts.yml | ConvertFrom-Yaml).'github.com'.oauth_token
-$headers = @{ Authorization = "token $token" }
+if (-Not $Path) {
+  if ($TeamName -match 'cloud-services-') {
+    $Path = "~/src/$($TeamName.Replace('cloud-services-',''))"
+  }
+  else {
+    $Path = "~/src/$TeamName"
+  }
+}
 
 if (-Not (Test-Path $Path)) {
   New-Item -Path $Path -ItemType Directory | Out-Null
 }
 
-Push-Location $Path
+$token=(Get-Content ~/.config/gh/hosts.yml | ConvertFrom-Yaml).'github.com'.oauth_token
+$headers = @{ Authorization = "token $token" }
 
-Invoke-RestMethod -Headers $headers "https://api.github.com/orgs/Equifax/teams/$TeamName/repos" | %{ $_.clone_url } | %{
-  Write-Host "Cloning $_"
-  git clone $_
+$repos = Invoke-RestMethod -Headers $headers "https://api.github.com/orgs/Equifax/teams/$TeamName/repos"
+foreach($repo in $repos) {
+  $clone_url = $repo.clone_url
+  $folder = Join-Path $Path $repo.name.Replace("$TeamName-",'')
+
+  Write-Host "Cloning $clone_url to $folder"
+  git clone $clone_url $folder
 }
-
-Pop-Location
--- .local/bin/init-repos.ps1
+++ .local/bin/init-repos.ps1
@@ -1,6 +1,6 @@
 #!/bin/pwsh
-clone-repos.ps1 cloud-services-equifax-shell src/equifax-shell
-clone-repos.ps1 cloud-services-container-imaging src/container-imaging
-clone-repos.ps1 cloud-services-kubernetes src/kubernetes
-clone-repos.ps1 cloud-services-kubernetes src/cloud-services
-clone-repos.ps1 iaas-platform-services-fr-smtp-relay src/smtp-relay
+clone-repos.ps1 cloud-services-equifax-shell
+clone-repos.ps1 cloud-services-container-imaging
+clone-repos.ps1 cloud-services-kubernetes
+clone-repos.ps1 cloud-services
+clone-repos.ps1 iaas-platform-services-fr-smtp-relay ~/src/smtp-relay
