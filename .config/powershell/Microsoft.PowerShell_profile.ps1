01ffdbe043160372795324a8ce984f09a3638102
Rob Cannon <rob.cannon@equifax.com>
2021-02-10 21:32:25 +0000

    Updated to only open current branch

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,6 @@ using namespace System.Management.Automation.Language
 
 Import-Module posh-git
 Import-Module oh-my-posh
-#Get-ChildItem /usr/local/share/powershell/Modules/oh-my-posh -Filter *.psm1 -Recurse | Import-Module
 
 Set-PoshPrompt -Theme $HOME/.config/oh-my-posh/my-posh.json
 
