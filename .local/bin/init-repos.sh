8227e1f3310b896a25d63bc77cd07a392ce043aa
Rob Cannon <rob@cannonsoftware.com>
2022-04-15 19:54:51 -0400

    Adding more repos to script

--- .local/bin/init-repos.sh
+++ .local/bin/init-repos.sh
@@ -5,3 +5,7 @@ mkdir -p ~/src/GitHub
 mkdir -p ~/src/RobCannon
 
 git clone https://github.com/RobCannon/dotfiles.git ~/src/RobCannon/dotfiles
+git clone https://github.com/RobCannon/flux-test.git ~/src/RobCannon/flux-test
+
+
+git clone https://github.com/stefanprodan/gitops-istio.git ~/src/GitHub/gitops-istio
