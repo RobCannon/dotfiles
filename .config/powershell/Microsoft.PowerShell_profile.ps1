cf5a22e1f17c72a931f2c19b50d6e9222f6a980f
Rob Cannon <rob@cannonsoftware.com>
2022-05-08 12:35:02 -0400

    Fixing powershell prompt

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,7 @@ if ([Environment]::GetCommandLineArgs().Length -gt 1) {
   return
 }
 
-oh-my-posh init pwsh --config $HOME/.config/oh-my-posh/my-pwsh.json -s | Invoke-Expression
+oh-my-posh init pwsh --config $HOME/.config/oh-my-posh/oh-my-posh.json -s | Invoke-Expression
 
 
 # This is an example profile for PSReadLine.
